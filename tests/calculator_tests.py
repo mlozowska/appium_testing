0e0d6505b2d40f3568b5a02b3855f5ad2c0fd644
Magdalena Lozowska <60215258+mlozowska@users.noreply.github.com>
2020-12-06 20:56:28 +0100

    calculator
    Tests for 4 basic operations for calculator (+, -, /, *), division by 0, sqr

--- tests/calculator_tests.py
+++ tests/calculator_tests.py
@@ -1,33 +1,45 @@
-from appium_day.pages.home_page import HomePage
+from pages.home_page import HomePage
 
 
 class TestCalculator:
 
-    def test_open_close_expert_panel(self, driver):
-        self.home_page = HomePage(driver)
-        self.home_page.open_expert_panel()
-        self.home_page.close_expert_panel()
+    # def test_open_close_expert_panel(self, driver):
+    #     self.home_page = HomePage(driver)
+    #     self.home_page.open_expert_panel()
+    #     self.home_page.close_expert_panel()
+    #
+    # def test_add(self, driver):
+    #     self.home_page = HomePage(driver)
+    #     self.home_page.add_numbers(2, 5)
+    #     result = self.home_page.get_result()
+    #     assert result == "7"
+    #
+    # def test_sub(self, driver):
+    #     self.home_page = HomePage(driver)
+    #     self.home_page.sub_numbers(5, 3)
+    #     result = self.home_page.get_result()
+    #     assert result == "2"
+    #
+    # def test_mul(self, driver):
+    #     self.home_page = HomePage(driver)
+    #     self.home_page.mul_numbers(2, 2)
+    #     result = self.home_page.get_result()
+    #     assert result == "4"
+    #
+    # def test_div(self, driver):
+    #     self.home_page = HomePage(driver)
+    #     self.home_page.div_numbers(0, 1)
+    #     result = self.home_page.get_result()
+    #     assert result == "0"
 
-    def test_add(self, driver):
+    def test_div_by_zero(self, driver):
         self.home_page = HomePage(driver)
-        self.home_page.add_numbers(2, 5)
-        result = self.home_page.get_result()
-        assert result == "7"
+        self.home_page.div_numbers(5, 0)
+        result = self.home_page.get_preview_result()
+        assert result == "Can't divide by 0"
 
-    def test_sub(self, driver):
+    def test_sqrt(self, driver):
         self.home_page = HomePage(driver)
-        self.home_page.sub_numbers(5, 3)
+        self.home_page.sqrt_number(9)
         result = self.home_page.get_result()
-        assert result == "2"
-
-    def test_mul(self, driver):
-        self.home_page = HomePage(driver)
-        self.home_page.mul_numbers(2, 2)
-        result = self.home_page.get_result()
-        assert result == "4"
-
-    def test_div(self, driver):
-        self.home_page = HomePage(driver)
-        self.home_page.div_numbers(0, 1)
-        result = self.home_page.get_result()
-        assert result == "0"
+        assert result == "3"
